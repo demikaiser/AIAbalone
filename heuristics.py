359abfba72afb1a1fcd470ba25fd1f4b16361aaa
Jake Jonghun Choi <demikaiser13@gmail.com>
2018-02-08 00:54:21 -0800

    Updates.

--- heuristics.py
+++ heuristics.py
@@ -9,10 +9,7 @@ Unauthorized copying of this file, via any medium is strictly prohibited.
 Written by Jake Jonghun Choi <jchoi179@my.bcit.ca>
 '''
 
-import view
 
-view = view.GUI()
-view.start_gui()
 
 
 
