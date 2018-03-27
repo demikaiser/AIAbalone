9cae64023f05e4073cdc1b3611da04af6b661b59
teehuang <tonyhuang1996@hotmail.ca>
2018-03-27 13:26:09 -0700

    eval function

--- ai_evaluation_function_Tony.py
+++ ai_evaluation_function_Tony.py
@@ -44,6 +44,173 @@ def get_evaluation_score(player, state):
 
     #TODO: Write your evaluation function(s) here.
 
+    #Value pieces in the middle over other pieces
+    def evaluation_function_middle(player,state):
+        counter1 = 0
+        counter2 = 0
+        for i in range(0,9):
+            for j in range(0,9):
+                if state[i][j] == player:
+                    counter1 += i - 5
+                    counter2 += j -5
+
+        total = (counter1 + counter2)*100
+        return total
+
+    #Check the edges and see where the piece is. Give a score according to how many pieces
+    #are on the corner
+
+    def evaluation_function_outer(player,state):
+        counter = 0
+        if state[0][4] == player:
+            counter+=1
+        if state[1][3] == player:
+            counter+=1
+        if state[2][2] == player:
+            counter+=1
+        if state[3][1] == player:
+            counter+=1
+        if state[4][0] == player:
+            counter+=1
+        if state[8][4] == player:
+            counter+=1
+        if state[7][5] == player:
+            counter+=1
+        if state[6][6] == player:
+            counter+=1
+        if state[5][7] == player:
+            counter+=1
+        if state[4][8] == player:
+            counter+=1
+
+        return counter*100
+
+    #Sumito move
+    #Evaluate the moves ahead to check if opponent pieces are endangering the edge pieces
+    def evaluation_function_sumito_ally(state):
+        counter = 0
+        if state[0][4] == 1:
+            if state[1][5] == 2:
+                if state[2][6] == 2:
+                    counter+=1
+        if state[1][3] == 1:
+            if state[2][4] == 2:
+                if state[3][5] == 2:
+                    counter+=1
+        if state[2][2] == 1:
+            if state[3][3] == 2:
+                if state[4][4] == 2:
+                    counter+=1
+        if state[3][1] == 1:
+            if state[4][2] == 2:
+                if state[5][3] == 2:
+                    counter+=1
+        if state[4][0] == 1:
+            if state[5][1] == 2:
+                if state[6][2] == 2:
+                    counter+=1
+        if state[8][4] == 1:
+            if state[7][3] == 2:
+                if state[6][2] == 2:
+                    counter+=1
+        if state[7][5] == 1:
+            if state[6][4] == 2:
+                if state[5][3] == 2:
+                    counter+=1
+        if state[6][6] == 1:
+            if state[5][5] == 2:
+                if state[4][4] == 2:
+                    counter+=1
+        if state[5][7] == 1:
+            if state[4][6] == 2:
+                if state[3][5] == 2:
+                    counter+=1
+        if state[4][8] == 1:
+            if state[3][7] == 2:
+                if state[2][6] == 2:
+                    counter+=1
+        return counter*100
+
+    # Sumito move
+    # Evaluate the moves ahead to check if ally pieces are endangering the edge pieces for the opponent
+    def evaluation_function_sumito_opponent(state):
+        counter = 0
+        if state[0][4] == 2:
+            if state[1][5] == 1:
+                if state[2][6] == 1:
+                    counter += 1
+        if state[1][3] == 2:
+            if state[2][4] == 1:
+                if state[3][5] == 1:
+                    counter += 1
+        if state[2][2] == 2:
+            if state[3][3] == 1:
+                if state[4][4] == 1:
+                    counter += 1
+        if state[3][1] == 2:
+            if state[4][2] == 1:
+                if state[5][3] == 1:
+                    counter += 1
+        if state[4][0] == 2:
+            if state[5][1] == 1:
+                if state[6][2] == 1:
+                    counter += 1
+        if state[8][4] == 2:
+            if state[7][3] == 1:
+                if state[6][2] == 1:
+                    counter += 1
+        if state[7][5] == 2:
+            if state[6][4] == 1:
+                if state[5][3] == 1:
+                    counter += 1
+        if state[6][6] == 2:
+            if state[5][5] == 1:
+                if state[4][4] == 1:
+                    counter += 1
+        if state[5][7] == 2:
+            if state[4][6] == 1:
+                if state[3][5] == 1:
+                    counter += 1
+        if state[4][8] == 2:
+            if state[3][7] == 1:
+                if state[2][6] == 1:
+                    counter += 1
+        return counter * 100
+
+    #evaluate all neighboring marbles and provide an evaluation score relative to the opponent's grouping
+    def evaluation_function_marble_grouping(player, state):
+        counter = 0
+        for i in range(1,8):
+            for j in range(1,8):
+                if state[i][j] == player:
+                    if state[i+1][j] == player:
+                        counter+=1
+                    if state[i-1][j] == player:
+                        counter+=1
+                    if state[i][j+1] == player:
+                        counter+=1
+                    if state[i][j-1] == player:
+                        counter+=1
+                    if state[i+1][j+1] == player:
+                        counter+=1
+                    if state[i+1][j-1] == player:
+                        counter+=1
+                    if state[i-1][j+1] == player:
+                        counter+=1
+                    if state[i-1][j-1] == player:
+                        counter+=1
+        return counter*50
+
+    score += evaluation_function_middle(ally,state)
+    score -= evaluation_function_middle(opponent,state)
+    score += evaluation_function_outer(opponent,state)
+    score -= evaluation_function_outer(ally, state)
+    score -= evaluation_function_sumito_ally(state)
+    score += evaluation_function_sumito_opponent(state)
+    score += evaluation_function_marble_grouping(ally, state)
+    score -= evaluation_function_marble_grouping(opponent, state)
+
     # Return the score evaluated.
     return score
+
 
